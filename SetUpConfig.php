ca61980b5bef948dd9b43b037d06448a95b0d4c8
TomW1996 <tomw09@live.co.uk>
2013-07-24 13:37:52 +0100

    Commenting

--- SetUpConfig.php
+++ SetUpConfig.php
@@ -1,5 +1,5 @@
 <?php
-	$sampleMass = $_POST['postSampleMass'];
+	$sampleMass = $_POST['postSampleMass'];	//Get data sent from main page and store in variables
 	$molWeight = $_POST['postMolWeight'];
 	$sampleEico = $_POST['postSampleEico'];
 	$blankEico = $_POST['postBlankEico'];
@@ -7,8 +7,8 @@
 	$eicoData = $_POST['postEicoData'];
 	$applyCorrection = $_POST['postApplyCorrection'];
 	
-	$fw = fopen('upload/config.txt', 'w');
-	fwrite($fw, "Raw Data File: upload/rawData.txt"."\n");
+	$fw = fopen('upload/config.txt', 'w');	//Create a new file to write to
+	fwrite($fw, "Raw Data File: upload/rawData.txt"."\n");	//Write config file to be used by python correction script, use values sent from main page, taken from input fields
 	fwrite($fw, "Gel Cap Data File: upload/gelcapData.txt"."\n");
 	if($eicoData != ""){
 		fwrite($fw, "Eicosane Data File: upload/eicoData.txt"."\n");	
@@ -38,5 +38,5 @@
 	}
 	fclose($fw);
 
-	shell_exec('C:\Python33\python.exe SQUID-Fix.py');
+	shell_exec('C:\Python33\python.exe SQUID-Fix.py');	//Run python correction script
 ?>
