d757b576aee056d88e75eb5fc9d060b08d9b54f2
TomW1996 <tomw09@live.co.uk>
2013-07-24 14:20:36 +0100

    Commenting

--- eicoUpload.php
+++ eicoUpload.php
@@ -1,33 +1,29 @@
 <?php
-	$allowedExts = array("csv", "dat", "txt");
-	$temp = explode(".", $_FILES["file"]["name"]);
-	$extension = end($temp);
-	if (($_FILES["file"]["size"] > 0) && ($_FILES["file"]["size"] < 20000) && in_array($extension, $allowedExts))
+	$allowedExts = array("csv", "dat", "txt");	//Array to store acceptable extensions - upload file can only be certain file types
+	$temp = explode(".", $_FILES["file"]["name"]);	//Split file name by .
+	$extension = end($temp);	//Get last section - extension
+	if (($_FILES["file"]["size"] > 0) && ($_FILES["file"]["size"] < 20000) && in_array($extension, $allowedExts)) //Check that file is 0-20000 bytes and has an acceptable extension
 	{
-		if($_FILES["file"]["error"] > 0){
-			print("Error: " . $_FILES["file"]["error"] . "<br>");
+		if($_FILES["file"]["error"] > 0){	//Are there any erors?
+			print("Error: " . $_FILES["file"]["error"] . "<br>");	//Print errors
 		}
-			else
-			{
-				print("Upload: " . $_FILES["file"]["name"] . "<br>");
-				print("Type: " . $_FILES["file"]["type"] . "<br>");
-				print("Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>");
-				print("Stored in: " . $_FILES["file"]["tmp_name"] . "<br>");
+		else
+		{
 			
-			if(file_exists("upload/" . $_FILES["file"]["name"]))
+			if(file_exists("upload/" . $_FILES["file"]["name"]))	//Check if file already exists
 			{
 				print($_FILES["file"]["name"] . " already exists. ");
 			}
-			else
-			{	
-				$fw = fopen("upload/eicoName.txt", "w");
+			else	//File does not already exist
+			{
+				$fw = fopen("upload/eicoName.txt", "w");	//Store original name of file in text document
 				fwrite($fw, $_FILES["file"]["name"]);
 				fclose($fw);
-				move_uploaded_file($_FILES["file"]["tmp_name"], "upload/eicoData.txt");
+				move_uploaded_file($_FILES["file"]["tmp_name"], "upload/eicoData.txt");	//Move the file to the upload folder
 			}
 		}
 	}
 	else{
-		print("Invalid file");
-	}	
+		//Invalid file
+	}
 ?>
