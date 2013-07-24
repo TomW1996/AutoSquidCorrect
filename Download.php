645e02f9447a57def9b54c22688871bb438c0312
TomW1996 <tomw09@live.co.uk>
2013-07-24 13:35:00 +0100

    Commenting

--- Download.php
+++ Download.php
@@ -1,13 +1,13 @@
 <?php	
-	if(file_exists("upload/rawName.txt")){
-		$fr = fopen("upload/rawName.txt", "r");
-		$name = fgets($fr);
-		fclose($fr);
-		$parts = explode('.', $name);
-		$name = $parts[0].'_Corrected.txt';
-		$file = $_POST['download'];
-		header('Content-type: text/plain');
-		header('Content-Disposition: attachment; filename='.$name.'');
-		readfile('upload/'.$file);
+	if(file_exists("upload/rawName.txt")){	//Check that file exists
+		$fr = fopen("upload/rawName.txt", "r");	//Open the file to read
+		$name = fgets($fr);	//Store the first line as $name
+		fclose($fr);	//Close file reader
+		$parts = explode('.', $name);	//Split $name between .
+		$name = $parts[0].'_Corrected.txt'; //Append _Corrected.txt to the first part of $name - name of downloaded file
+		$file = $_POST['download'];	//Get location of download file
+		header('Content-type: text/plain');	//Set type
+		header('Content-Disposition: attachment; filename='.$name.'');	//Set name
+		readfile('upload/'.$file);	//Download the file
 	}
 ?>
